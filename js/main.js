d2801ee2d5bba7d7357dc476ca1186908f41f14d
JensHaugaard1 <136031062+JensHaugaard1@users.noreply.github.com>
2023-12-08 20:23:55 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -81,7 +81,7 @@ function animate() {
   if (object && objToRender === "eye") {
     //I've played with the constants here until it looked good 
     
-    scene.rotation.x =  (mouseY / window.innerHeight) - 0.5;
+    scene.rotation.x =  (mouseY * 0.8 / window.innerHeight) - 0.4;
     //object.rotation.y += 0.01;
     //object.rotation.y += 0.01;
     object.rotation.y =  + scrollY / 750;
